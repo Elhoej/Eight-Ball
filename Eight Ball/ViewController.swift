e7c168e7f75a4a5222fe543f827bd5f1f32343ea
Simon Elhoej <simonelhoej@gmail.com>
2018-07-24 18:59:29 +0200

    Initial commit

---
+++ Eight Ball/ViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  Eight Ball
+//
+//  Created by Simon Elhoej Steinmejer on 24/07/18.
+//  Copyright © 2018 Simon Elhoej Steinmejer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    private let answers = [
+        "It is certain.",
+        "It is decidedly so.",
+        "Without a doubt.",
+        "Yes - definitely.",
+        "You may rely on it.",
+        "As I see it, yes.",
+        "Most likely.",
+        "Outlook good.",
+        "Yes.",
+        "Signs point to yes.",
+        "Reply hazy, try again",
+        "Ask again later.",
+        "Better not tell you now.",
+        "Cannot predict now.",
+        "Concentrate and ask again.",
+        "Don't count on it.",
+        "My reply is no.",
+        "My sources say no",
+        "Outlook not so good.",
+        "Very doubtful.",
+        ]
+    
+
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    @IBAction func flipEightBall(_ sender: Any)
+    {
+        let randomIndex = Int(arc4random_uniform(UInt32(answers.count)))
+        answerLabel.text = answers[randomIndex]
+    }
+    
+    
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
